fba1197647be417ddb8e13af19c51f3fdcdb92b0
dmitriypetukhov <pdnsite@gmail.com>
2021-07-19 19:10:55 +0300

    Методы some и map

--- test4/script.js
+++ test4/script.js
@@ -3,7 +3,25 @@ const arr = [1, 5, 10, 20];
 // const filteredArr = arr.filter(item => item > 10);
 // console.log(filteredArr);
 
-arr.myFilter = cbFn => {
+// как метод
+arr.myFilter = function(cbFn) {
+	const newArr = [];
+
+	this.forEach(item => {
+		if (cbFn(item)) {
+			newArr.push(item);
+		}
+	});
+
+	return newArr;
+};
+
+const myFilteredArr = arr.myFilter(item => item > 10);
+
+// console.log(myFilteredArr);
+
+// как функция
+const myFilter = (arr, cbFn) => {
 	const newArr = [];
 
 	arr.forEach(item => {
@@ -15,6 +33,32 @@ arr.myFilter = cbFn => {
 	return newArr;
 };
 
-const myFilteredArr = arr.myFilter(item => item > 10);
+const myFilteredArr2 = myFilter(arr, item => item > 10);
+console.log(myFilteredArr2);
 
-console.log(myFilteredArr);
+arr.mySome = function(cbFn) {
+	let result = false;
+
+	this.forEach(item => {
+		if (cbFn(item)) {
+			result = true;
+		}
+	});
+	
+	return result;
+};
+
+console.log(arr.mySome(item => item === 20));
+
+arr.myMap = function(cbFn) {
+	const newArr = [];
+
+	this.forEach(item => {
+		newArr.push(cbFn(item));
+	});
+
+	return newArr;
+};
+
+const mapped = arr.myMap(item => item = item * 2);
+console.log(mapped);
